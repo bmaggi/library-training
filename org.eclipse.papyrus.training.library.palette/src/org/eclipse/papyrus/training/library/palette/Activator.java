37fca70f2a98e2f0e932f05a4af8b21c44ede6d9
Benoit maggi <benoit.maggi@cea.fr>
2016-09-06 15:45:28 +0200

    Add Periodical Monthly in New Chlid Menu
    Signed-off-by: Benoit maggi <benoit.maggi@cea.fr>

--- org.eclipse.papyrus.training.library.palette/src/org/eclipse/papyrus/training/library/palette/Activator.java
+++ org.eclipse.papyrus.training.library.palette/src/org/eclipse/papyrus/training/library/palette/Activator.java
@@ -1,4 +1,15 @@
-package org.eclipse.papyrus.training.library.model;
+/*****************************************************************************
+ * Copyright (c) 2016 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Benoit Maggi benoit.maggi@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.training.library.palette;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -9,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.training.library.model"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.training.library.palette"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
