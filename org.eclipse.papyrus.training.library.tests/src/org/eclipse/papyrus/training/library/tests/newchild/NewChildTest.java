deb9fd0380ef6916dea35874d45f71af8038d14a
Benoit maggi <benoit.maggi@cea.fr>
2016-09-11 12:13:38 +0200

    Add cocoa conf and tests
    Signed-off-by: Benoit maggi <benoit.maggi@cea.fr>

--- org.eclipse.papyrus.training.library.tests/src/org/eclipse/papyrus/training/library/tests/newchild/NewChildTest.java
+++ org.eclipse.papyrus.training.library.tests/src/org/eclipse/papyrus/training/library/tests/newchild/NewChildTest.java
@@ -12,7 +12,21 @@
 package org.eclipse.papyrus.training.library.tests.newchild;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.papyrus.infra.newchild.elementcreationmenumodel.CreationMenu;
+import org.eclipse.papyrus.infra.services.edit.service.ElementEditServiceUtils;
+import org.eclipse.papyrus.infra.types.core.registries.ElementTypeSetConfigurationRegistry;
 import org.eclipse.papyrus.training.library.tests.AbstractEMFResourceTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -24,6 +38,12 @@ public class NewChildTest extends AbstractEMFResourceTest {
 
 	public static final String PATH = org.eclipse.papyrus.training.library.newchild.Activator.PLUGIN_ID + "/resources/newChild.creationmenumodel";
 
+	@BeforeClass
+	public static void loadElementTypeSet(){
+		ElementTypeSetConfigurationRegistry.getInstance();
+	}
+	
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -37,4 +57,35 @@ public class NewChildTest extends AbstractEMFResourceTest {
 		doValidateResource();
 	}
 
+	/**
+	 * Check that referenced element type are in the registry
+	 */
+	@Test
+	public void checkMenuNewChildElementTypeIdRefs() {
+		URI createPlatformPluginURI = URI.createPlatformPluginURI(PATH, true);
+		ResourceSetImpl resourceSetImpl = new ResourceSetImpl();
+		Resource resource = resourceSetImpl.getResource(createPlatformPluginURI, true);
+
+		TreeIterator<EObject> allContents = resource.getAllContents();
+		while (allContents.hasNext()) {
+			EObject eObject = (EObject) allContents.next();
+
+			if (eObject instanceof CreationMenu) {
+				CreationMenu p = (CreationMenu) eObject;
+				String elementTypeIdRef = p.getElementTypeIdRef();
+				Assert.assertTrue("Unregistred element type id : " + elementTypeIdRef, ElementEditServiceUtils.getEditServiceProvider().isKnownElementType(elementTypeIdRef));
+
+				String iconPath = p.getIcon();
+				 if (iconPath != null && !"".equals(iconPath)){
+						try {
+							URL url = new URL(iconPath);
+							Assert.assertNotNull("The icon "+iconPath+"(for : "+elementTypeIdRef+") can't be found", FileLocator.find(url));
+						} catch (MalformedURLException e) {
+							Assert.fail("The new child menu is refering to a malformed url "+iconPath);
+						}
+				 }
+			}
+		}
+	}
+	
 }
--- org.eclipse.papyrus.training.library.tests/src/org/eclipse/papyrus/training/library/tests/palette/PaletteTest.java
+++ org.eclipse.papyrus.training.library.tests/src/org/eclipse/papyrus/training/library/tests/palette/PaletteTest.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2015 CEA LIST.
+ * Copyright (c) 2016 CEA LIST.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,7 +12,27 @@
 package org.eclipse.papyrus.training.library.tests.palette;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.papyrus.infra.services.edit.service.ElementEditServiceUtils;
+import org.eclipse.papyrus.infra.types.core.registries.ElementTypeSetConfigurationRegistry;
 import org.eclipse.papyrus.training.library.tests.AbstractEMFResourceTest;
+import org.eclipse.papyrus.uml.diagram.paletteconfiguration.ChildConfiguration;
+import org.eclipse.papyrus.uml.diagram.paletteconfiguration.DrawerConfiguration;
+import org.eclipse.papyrus.uml.diagram.paletteconfiguration.ElementDescriptor;
+import org.eclipse.papyrus.uml.diagram.paletteconfiguration.IconDescriptor;
+import org.eclipse.papyrus.uml.diagram.paletteconfiguration.PaletteConfiguration;
+import org.eclipse.papyrus.uml.diagram.paletteconfiguration.ToolConfiguration;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -24,6 +44,15 @@ public class PaletteTest extends AbstractEMFResourceTest {
 
 	public static final String PALETTE_PATH = org.eclipse.papyrus.training.library.palette.Activator.PLUGIN_ID+"/resources/extlibrary.classdiagram.paletteconfiguration";
 
+
+	public static final String NODE = "node"; 
+
+	public static final String EDGE = "edge"; 
+
+	private String rootPath = "org.eclipse.papyrus.training.library"; 
+
+
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -35,6 +64,70 @@ public class PaletteTest extends AbstractEMFResourceTest {
 	@Test
 	public void validateResource() {
 		doValidateResource();
+	}
+	
+	@Before
+	public void loadElementTypeRegistry(){
+		ElementTypeSetConfigurationRegistry.getInstance();
+	}
+	
+	public boolean assertPaletteId(String id) {
+		return id != null && (id.startsWith(rootPath + "." + NODE) || id.startsWith(rootPath + "." + EDGE));
+	}
+	
+	
+	/**
+	 * Check palette ids and element type reference
+	 */
+	@Test
+	public void checkPaletteIds() {
+		URI createPlatformPluginURI = URI.createPlatformPluginURI(PALETTE_PATH, true);
+		ResourceSetImpl resourceSetImpl = new ResourceSetImpl();
+		Resource resource = resourceSetImpl.getResource(createPlatformPluginURI, true);
+
+
+		TreeIterator<EObject> allContents = resource.getAllContents();
+		while (allContents.hasNext()) {
+			EObject eObject = (EObject) allContents.next();
+
+			if (eObject instanceof PaletteConfiguration) {
+				PaletteConfiguration p = (PaletteConfiguration) eObject;
+				EList<DrawerConfiguration> drawerConfigurations = p.getDrawerConfigurations();
+				for (DrawerConfiguration drawerConfiguration : drawerConfigurations) {
+					EList<ChildConfiguration> ownedConfigurations = drawerConfiguration.getOwnedConfigurations();
+					for (ChildConfiguration childConfiguration : ownedConfigurations) {
+
+						// check id
+						String id = childConfiguration.getId();
+						Assert.assertTrue("Malformed palette id : " + id, assertPaletteId(id));
+
+						if (childConfiguration instanceof ToolConfiguration) {
+							ToolConfiguration toolConfiguration = (ToolConfiguration) childConfiguration;
+							
+							// check that the referenced elementtype exist
+							EList<ElementDescriptor> elementDescriptors = toolConfiguration.getElementDescriptors();
+							for (ElementDescriptor elementDescriptor : elementDescriptors) {
+								String elementTypeId = elementDescriptor.getElementTypeId();
+								Assert.assertTrue("Unregistred element id : " + elementTypeId, ElementEditServiceUtils.getEditServiceProvider().isKnownElementType(elementTypeId));
+							}
+							
+							// check icon file
+							IconDescriptor icon = toolConfiguration.getIcon();
+							String iconPath = "platform:/plugin/"+ icon.getPluginID()+"/"+  icon.getIconPath();
+							 if (iconPath != null && !"".equals(iconPath)){
+									try {
+										URL url = new URL(iconPath);
+										Assert.assertNotNull("The icon "+iconPath+"(for : "+toolConfiguration.getId()+") can't be found", FileLocator.find(url));
+									} catch (MalformedURLException e) {
+										Assert.fail("The new child menu is refering to a malformed url "+iconPath);
+									}
+							 }	
+							 
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
